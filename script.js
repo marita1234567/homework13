c0dc227dd0e0eb2b03f0186fc2730525f2d84925
marita1234567 <m.bibichadze@sabauni.edu.ge>
2024-10-17 19:02:38 +0400

    work13

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = "marita"
+const lastname =   ",  bibichadze"
+const email = ",  m.bibichadze@.edu.ge"
+const all = name + lastname + email;
+console. log(all)
+let age = 25
+console. log (age)
+const phone = "551109931"
+console. log(`%c ${phone}`, `color: red`);
+const adult = true
+console. log (`%c ${adult}`, `color: yellow`);
